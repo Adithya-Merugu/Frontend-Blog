aef8bd3b620bc191f6fc996c84a9fdcdc1041899
Adithya <adithya2003porps@gmail.com>
2023-09-07 13:16:16 +0530

    edited write

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -28,7 +28,8 @@ export default function Write() {
     }
     try {
       const res = await axios.post("https://bloged.onrender.com/api/posts/", newPost);
-      window.location.replace("/post/" + res.data._id);
+      window.location.replace("/");
+     /* ("/post/" + res.data._id);*/
     } catch (err) {}
   };
   return (
